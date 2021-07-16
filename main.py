1d03d8685c7dd606841d6b58531ab8354d124bc2
unknown <gopal525shaurya@gmail.com>
2021-07-16 22:46:59 +0530

    Ho gya qr code complete jai shri ram

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+#from pydantic import BaseModel
 
+import pyqrcode
+import io
+from starlette.responses import StreamingResponse
+import png
 app = FastAPI()
 
 @app.get('/')
@@ -7,3 +12,11 @@ def first():
     return {
         'message':'I am doing it 😁'
     }
+
+@app.post("/image")
+def qrCodeImage(stringInp):
+    qrcode2 = pyqrcode.create(stringInp)
+    #return StreamingResponse(io.BytesIO(qrcode2.tobytes()), media_type="image/png")
+    qrcode2.png("myqr.png",scale=8)
+    val=open("myqr.png", mode="rb")
+    return StreamingResponse(val, media_type="image/png")
